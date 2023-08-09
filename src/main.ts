b81cb5184f8c0287b98eef643bae66139c6d5fc0
Akif Adak <adakakif@gmail.com>
2023-08-09 21:19:19 +0300

    refactor: :recycle: Change to standalone

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { importProvidersFrom } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app/app.component";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+  ],
+  // eslint-disable-next-line no-console, @typescript-eslint/no-explicit-any
+}).catch((err: any) => console.error(err));
