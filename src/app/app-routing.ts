4730a1e9913eb583e827ac5561dc16ab77c4cd76
Akif Adak <adakakif@gmail.com>
2023-08-10 18:50:08 +0300

    feat: :sparkles: Create 'app-routing'

---
+++ src/app/app-routing.ts
@@ -0,0 +1,26 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { WatchesComponent } from "./components/watches/watches.component";
+
+const APP_ROUTES: Routes = [
+  {
+    path: "watches",
+    component: WatchesComponent,
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "home",
+  },
+  {
+    path: "**",
+    pathMatch: "full",
+    redirectTo: "home",
+  },
+];
+
+export default APP_ROUTES;
