3cfe89df159ede2ea9a1977cd327f1a9083caebb
Akif Adak <adakakif@gmail.com>
2023-08-10 18:44:02 +0300

    feat(components): :sparkles: Create 'watches.component'

---
+++ src/app/components/watches/watches.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-watches',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './watches.component.html',
+  styleUrls: ['./watches.component.scss']
+})
+export class WatchesComponent {
+
+}
